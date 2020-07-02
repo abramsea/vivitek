66069b8960fe00f3156573d4ed05ec5088e57a4b
Evgenii Abramson <5366163-abramsea@users.noreply.gitlab.com>
2020-07-02 16:34:31 +0300

    desktop-done

---
+++ js/switcher.js
@@ -0,0 +1,19 @@
+$('#q6-b').click( () => {
+    $('#q6-img').attr('src','images/Q6_Main_image_black.png')
+    $('#q6-button').attr('href', 'https://pult.ru/product/proektor-vivitek-qimi-q6-black')
+})
+
+$('#q6-w').click( () => {
+    $('#q6-img').attr('src','images/Q6_Main_image_white.png')
+    $('#q6-button').attr('href', 'https://pult.ru/product/proektor-vivitek-qimi-q6-white')
+})
+
+$('#q8-b').click( () => {
+    $('#q8-img').attr('src','images/q8.jpg')
+    $('#q8-button').attr('href', 'https://pult.ru/product/proektor-vivitek-qumi-q8–bk')
+})
+
+$('#q8-w').click( () => {
+    $('#q8-img').attr('src','images/q8w.jpg')
+    $('#q8-button').attr('href', 'https://pult.ru/product/proektor-vivitek-qumi-q8–wh')
+})
